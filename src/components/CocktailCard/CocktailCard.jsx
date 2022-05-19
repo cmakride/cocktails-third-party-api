4f912b10103eb5380bb75a819f0eafb5dd639f1b
ChrisMak <cmakride@gmail.com>
2022-05-18 20:29:38 -0400

    Cocktail List

---
+++ src/components/CocktailCard/CocktailCard.jsx
@@ -0,0 +1,9 @@
+const CocktailCard = () => {
+  return ( 
+    <>
+    <h1>Im a Cocktail</h1>
+    </>
+   );
+}
+ 
+export default CocktailCard;
--- src/pages/CocktailList/CocktailList.jsx
+++ src/pages/CocktailList/CocktailList.jsx
@@ -1,24 +1,44 @@
-import Cocktail from "../../components/Cocktail/Cocktail";
 import { useState, useEffect } from "react";
 import { getCocktails } from "../../services/api-calls";
+import CocktailCard from "../../components/CocktailCard/CocktailCard";
 
 
 
 const CocktailList = (props) => {
-  const [cocktails,setcocktails]=useState([])
+  const [cocktails, setcocktails] = useState([])
 
-  useEffect(()=>{
+  const cocktailArray = cocktails.drinks
+
+  useEffect(() => {
     getCocktails()
-    .then(cocktailsData => setcocktails(cocktailsData))
-  },[])
+      .then(cocktailsData => setcocktails(cocktailsData))
+  }, [])
+
+  console.log(cocktailArray)
 
 
-  return ( 
+  return (
+    cocktailArray ?
     <>
-    <h2>COCKTAIL LIST PAGE</h2>
-    <Cocktail/>
+    
+      <h1 className="my-8 text-back text-center text-3xl font-bold p-4">All Cocktails</h1>
+      <div className="grid grid-cols-1 gap-4 mx-5 justify-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 ">
+        {cocktailArray.map((cocktail, idx) => (
+
+          <div className="h-96 w-96 max-w-xs border-2 border-gray-200 border-opacity-600 rounded-lg overflow-hidden bg-gradient-to-r from-light-100 to-dark-100" key={idx}>
+
+            <CocktailCard cocktail={cocktail}
+            />
+
+          </div>
+        ))}
+      </div>
     </>
-   );
+    :
+    <>
+    <h4>Loading Cocktails</h4>
+    </>
+  );
 }
- 
+
 export default CocktailList;
