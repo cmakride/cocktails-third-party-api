fa64181cb4f065a0fd6233af8f26d8d6abd95bc5
ChrisMak <cmakride@gmail.com>
2022-05-19 15:17:24 -0400

    Cocktail Details

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Cocktail Recipe Finder
+This App was built to be an easy and fast way to lookup cocktail recipes. It connected to a third party API Thecocktaildb.com/
+
+## Technologies used
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
+![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white)
+![Netlify](https://img.shields.io/badge/netlify-%23000000.svg?style=for-the-badge&logo=netlify&logoColor=#00C7B7)
--- src/pages/CocktailDetail/CocktailDetail.jsx
+++ src/pages/CocktailDetail/CocktailDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { getDetails } from '../../services/api-calls';
 
 const CocktailDetail = () => {
@@ -11,23 +11,53 @@ const CocktailDetail = () => {
 
   const cocktailID = location.state.drinkId
 
-  const details = cocktailDetails.drinks
+  const details = cocktailDetails
+  
+
+  console.log(details)
+
+
 
   useEffect(() => {
     getDetails(cocktailID)
-      .then(cocktailDetails => setCocktailDetails(cocktailDetails))
+      .then(cocktailDetails => setCocktailDetails(cocktailDetails.drinks[0]))
   }, [cocktailID])
 
-  return ( 
-    details ? 
-    <>
-    
-    </>
-    :
-    <>
-    <h2>Loading Details</h2>
-    </>
-   );
+  return (
+    details ?
+      <>
+        <div className='py-8 w-full flex justify-center items-center'>
+          <div className="bg-white rounded w-11/12 shadow hover:shadow-md duration-4 flex flex-col justify-center items-center">
+            <div className="flex flex-row justify-between uppercase font-bold border-b p-6">
+              <p>{details.strDrink}</p>
+            </div>
+
+            <div className="p-6 flex flex-col items-center gap-1 mb-3">
+              <img className="w-96 flex self-center rounded shadow-lg mb-6" src={details.strDrinkThumb} alt="Cocktail" />
+              <div className='flex gap-3 items-center'>
+               
+                
+              </div>
+              <h2>Method: {details.strInstructions}</h2>
+              <h2>Served in: {details.strGlass}</h2>
+              <h2>Ingredients:</h2>
+              {/* {currentCocktail.ingredients.map((ingredient, idx) => (
+                <div key={idx} >
+                  {ingredient}
+                </div>
+
+              ))} */}
+
+
+            </div>
+          </div>
+        </div>
+      </>
+      :
+      <>
+        <h2>Loading Details</h2>
+      </>
+  );
 }
- 
+
 export default CocktailDetail;
