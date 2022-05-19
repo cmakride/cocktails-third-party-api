e72920d09531625ac38b8633fcb4e63d9b49e4b2
ChrisMak <cmakride@gmail.com>
2022-05-19 19:07:11 -0400

    Cocktail Details Ingredients and Amounts States

--- src/pages/CocktailDetail/CocktailDetail.jsx
+++ src/pages/CocktailDetail/CocktailDetail.jsx
@@ -4,23 +4,49 @@ import { getDetails } from '../../services/api-calls';
 
 const CocktailDetail = () => {
   const [cocktailDetails, setCocktailDetails] = useState({})
+  const [ingredientsArray, setIngredientsArray] = useState([])
+  const [amountsArray, setAmountsArray] = useState([])
+
 
   let location = useLocation()
-
   // ! getting the ID from location state passed down from cocktailCard
-
   const cocktailID = location.state.drinkId
 
   const details = cocktailDetails
-  
-
-  console.log(details)
-
-
 
   useEffect(() => {
     getDetails(cocktailID)
-      .then(cocktailDetails => setCocktailDetails(cocktailDetails.drinks[0]))
+      .then(cocktailDetails => {
+        setCocktailDetails(cocktailDetails.drinks[0])
+        const tempdetails = cocktailDetails
+        const tempDet = tempdetails.drinks[0]
+        
+        const arrayDetails = Object.values(tempDet)
+        console.log(arrayDetails)
+
+        let tempArray = []
+        let tempArray2 = []
+        for (let i = 1; i < 16; i++) {
+          if (arrayDetails[16+i] === null) {
+
+          } else {
+
+            tempArray.push(arrayDetails[16 + i])
+          }
+        }
+        for (let i = 1; i < 16; i++) {
+          if (arrayDetails[31+i] === null) {
+
+          } else {
+            tempArray2.push(arrayDetails[31 + i])
+          }
+        }
+        console.log(tempArray)
+        setIngredientsArray(tempArray)
+        console.log(tempArray2)
+        setAmountsArray(tempArray2)
+
+      })
   }, [cocktailID])
 
   return (
@@ -35,18 +61,33 @@ const CocktailDetail = () => {
             <div className="p-6 flex flex-col items-center gap-1 mb-3">
               <img className="w-96 flex self-center rounded shadow-lg mb-6" src={details.strDrinkThumb} alt="Cocktail" />
               <div className='flex gap-3 items-center'>
-               
-                
+
+
               </div>
               <h2>Method: {details.strInstructions}</h2>
               <h2>Served in: {details.strGlass}</h2>
               <h2>Ingredients:</h2>
-              {/* {currentCocktail.ingredients.map((ingredient, idx) => (
-                <div key={idx} >
-                  {ingredient}
-                </div>
+              {/* {ingredientsArray ?
+                <>
+                  {ingredientsArray.map((ingredientArray, idx) => (
 
-              ))} */}
+                    true ?
+                      <>
+                        <div key={idx} >
+                          {ingredientArray}
+                        </div>
+                      </>
+                      :
+                      <>
+                      </>
+
+
+                  ))}
+                </>
+                :
+                <>
+                </>
+              } */}
 
 
             </div>
