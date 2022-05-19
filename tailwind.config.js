270b47785c95533ad9fcc2d505c6f07758e95c08
ChrisMak <cmakride@gmail.com>
2022-05-18 20:00:30 -0400

    Tailwind Install

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+
+        'home': "url('./assets/home.png')",
+
+      },
+      fontFamily:{
+        'main':['"Taviraj"', 'serif']
+        
+      },
+      colors: {
+        rosebrown: {
+          100: 'rgb(187,152,148)',
+          200: 'rgb(224,206,206)',
+          300: 'rgb(146,123,144)',
+          500: 'rgb(87,71,85)'
+        }
+      }
+    }
+  },
+  plugins: [],
+}
