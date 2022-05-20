1f5f6daf14cf8c52ddde37e51dcf14582a3dac6f
ChrisMak <cmakride@gmail.com>
2022-05-19 20:29:48 -0400

    Search Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import CocktailList from './pages/CocktailList/CocktailList';
 import CocktailDetail from './pages/CocktailDetail/CocktailDetail';
+import Search from './components/Search/Search';
 
 function App() {
   return (
     <>
-    <a href='/cocktails'>To All Cocktails</a>
+    <a href='/cocktails'>All Cocktails</a>
+    <a href='/'>Search</a>
       <Routes>
+        <Route path='/' element={<Search/>}/>
         <Route path='/cocktails' element={<CocktailList />} />
         <Route path='cocktail' element={<CocktailDetail/>}/>
       </Routes>
