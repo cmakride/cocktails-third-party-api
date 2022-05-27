33b888ce0db343f71a29104155a363fca1a09c35
ChrisMak <cmakride@gmail.com>
2022-05-27 18:23:36 -0400

    NavBar Update

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,15 +13,6 @@ const NavBar = () => {
 
             </NavLink>
           </div>
-          <div className="block lg:hidden ">
-            <button
-              id="nav"
-              className="flex items-center px-3 py-2 border-2 rounded text-blue-700 border-blue-700 hover:text-blue-700 hover:border-blue-700">
-              <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
-                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-              </svg>
-            </button>
-          </div>
         </div>
 
         <div className="menu w-full flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8">
